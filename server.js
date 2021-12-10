607f0c9f94d734a6c249046257b17aa4b3758f71
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-09 20:03:39 -0800

    replaced specific  routes with general connection to router

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false,
 });
 
-// routes for completing CRUD
-app.use(require("./routes/api.js"));
-//responsible for serving the html files
-//exercise.js and stats.js
-app.use(require("./routes/view.js"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
