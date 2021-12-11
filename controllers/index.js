a2bef15a337a9b29545fcabf02acc8ffe7372940
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-10 17:00:23 -0800

    added index.js to create connection for database seeds

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const fitnessRoutes = require('./);
+
+router.use('/', fitnessRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
+
+
+// // routes for completing CRUD
+// app.use(require("./controllers/api.js.js"));
+// //responsible for serving the html files
+// //exercise.js and stats.js
+// app.use(require("./controllers/view.js.js"));
