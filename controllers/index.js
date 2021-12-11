39ccb3b4d2102c17b2baba52c36b861e5c78c10d
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-11 13:17:19 -0800

    modified fitnessRoutes to correct path

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const fitnessRoutes = require('./);
+const apiRoutes = require("./api");
+const fitnessRoutes = require("./fitness");
 
-router.use('/', fitnessRoutes);
-router.use('/api', apiRoutes);
+router.use("/", fitnessRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
-
 
 // // routes for completing CRUD
 // app.use(require("./controllers/api.js.js"));
