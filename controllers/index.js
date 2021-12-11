4cfd2fd90370809144018d9664d20945c85f8d53
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-11 14:34:08 -0800

    modified mongoose.connect to connecto to heroku

--- controllers/index.js
+++ controllers/index.js
@@ -7,9 +7,3 @@ router.use("/", fitnessRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
-
-// // routes for completing CRUD
-// app.use(require("./controllers/api.js.js"));
-// //responsible for serving the html files
-// //exercise.js and stats.js
-// app.use(require("./controllers/view.js.js"));
